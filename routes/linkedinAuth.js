5a81b6f0c63a85cc528d6ab10e4666e1d5f77fe6
Princeton Henry <phenry9@gatech.edu>
2020-04-09 18:26:33 -0400

    cleaned linkedInAuth. removed unnecesary functions

--- routes/linkedinAuth.js
+++ routes/linkedinAuth.js
@@ -1,38 +1,6 @@
-var express = require('express')
-  , http = require('http')
-  , passport = require('passport')
-  , util = require('util')
-  , LinkedInStrategy = require('passport-linkedin').Strategy;
-
-var app = express();
+var passport = require('passport');
+var express = require('express');
 var router = express.Router();
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var session = require('express-session');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
-
-//app.use(express.logger());
-//app.use(express.cookieParser());
-// app.use(express.bodyParser());
-//app.use(express.methodOverride());
-// app.use(express.session({ secret: 'keyboard cat' }));
-
-app.use(logger('dev'));
-app.use(methodOverride('X-HTTP-Method-Override'));
-app.use(cookieParser("keyboard cat"));
-app.use(bodyParser.urlencoded({ extended: false})); //extended false??
-app.use(session({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: true }
-  }))
-
-// Initialize Passport!  Also use passport.session() middleware, to support
-// persistent login sessions (recommended).
-app.use(passport.initialize());
-app.use(passport.session());
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
@@ -77,47 +45,6 @@ router.get('/logout', function(req, res){
     req.logout();
     res.redirect('/');
 });
-
-app.use(express.static(__dirname + '/public'));
-
-var LINKEDIN_API_KEY = "860jdfnwzxfu4z";
-var LINKEDIN_SECRET_KEY = "48T071Z7h7x4UjEN";
-
-// Passport session setup.
-//   To support persistent login sessions, Passport needs to be able to
-//   serialize users into and deserialize users out of the session.  Typically,
-//   this will be as simple as storing the user ID when serializing, and finding
-//   the user by ID when deserializing.  However, since this example does not
-//   have a database of user records, the complete LinkedIn profile is
-//   serialized and deserialized.
-passport.serializeUser(function(user, done) {
-    done(null, user);
-  });
-  
-passport.deserializeUser(function(obj, done) {
-    done(null, obj);
-});
-
-// Use the LinkedInStrategy within Passport.
-//   Strategies in passport require a `verify` function, which accept
-//   credentials (in this case, a token, tokenSecret, and LinkedIn profile), and
-//   invoke a callback with a user object.
-passport.use(new LinkedInStrategy({
-    consumerKey: LINKEDIN_API_KEY,
-    consumerSecret: LINKEDIN_SECRET_KEY,
-    callbackURL: "http://127.0.0.1:3000/auth/linkedin/callback"
-  },
-  function(token, tokenSecret, profile, done) {
-    // asynchronous verification, for effect...
-    process.nextTick(function () {
-      // To keep the example simple, the user's LinkedIn profile is returned to
-      // represent the logged-in user.  In a typical application, you would want
-      // to associate the LinkedIn account with a user record in your database,
-      // and return that user instead.
-      return done(null, profile);
-    });
-  }
-));
 
 // Simple route middleware to ensure user is authenticated.
 //   Use this route middleware on any resource that needs to be protected.  If
