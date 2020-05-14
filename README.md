cecf2244ce517458646a7d9810e1eed391e88b34
Princeton Henry <phenry9@gatech.edu>
2020-05-14 11:09:18 -0400

    Added npm commands list to README.md

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ In order to run the server, type 'npm start' in the command line
 in the directory the Knightley Project is saved.
 
 The server will be on http://localhost:3000/
+
+#Commands
+##npm start : start development server
+
+##npm test : runs all test cases in /test
+
+##npm run-script coverage  runs all test cases in /test and includes coverage report
--- test/db.test.js
+++ test/db.test.js
@@ -3,5 +3,10 @@ const expect = require('chai').expect
 const request = require('supertest');
 const app = require('../app');
 
-      //Database unit tests
-        //Make sure query, insert, and delete work
+describe("Unit test for databse functions", function() {
+
+  //unit test
+  it("should return matching users"); //query
+  it("should insert users into databse");
+  it("should delete user with specified user name");
+});
